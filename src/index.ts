3d85a86e78bf09c67afe498d1a0c00ea70e78c9d
Drizzy-hub <Paulosdahprogrammer@gmail.com>
2022-12-04 13:00:49 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import express from "express";
 import pg from "pg";
 
 // Connect to the database using the DATABASE_URL environment
+
 //   variable injected by Railway
 const pool = new pg.Pool();
 
@@ -13,9 +14,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
 app.use(bodyParser.text({ type: "text/html" }));
 
-app.get("/", async (req, res) => {
-  const { rows } = await pool.query("SELECT NOW()");
-  res.send(`Hello, World! The time from the DB is ${rows[0].now}`);
+// app.get("/", async (req, res) => {
+//   const { rows } = await pool.query("SELECT NOW()");
+//   res.send(`Hello, World! The time from the DB is ${rows[0].now}`);
+// });
+app.get('/tickets', async(req, res) => {
+  try {
+    const allTickets = await pool.query('SELECT * FROM ticketsys');
+    res.json(allTickets.rows);
+  } catch (err) {
+    console.log('there is no ticket yet');
+  }
 });
 
 app.listen(port, () => {
